a56d95a5a4c197862b8677f673576bcacdd970e6
Ivan Budiselic <ibudiselic@gmail.com>
2020-03-25 23:08:26 +0100

    Switch to new datasource as the current one got deprecated (recovered cases unavailable now).

--- data/update_data.ps1
+++ data/update_data.ps1
@@ -1,3 +1,2 @@
-Invoke-WebRequest -OutFile confirmed.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
-Invoke-WebRequest -OutFile dead.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv
-Invoke-WebRequest -OutFile recovered.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv
+Invoke-WebRequest -OutFile confirmed.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
+Invoke-WebRequest -OutFile dead.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
