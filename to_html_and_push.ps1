b920ba7b52f06ffee1c2e9236ec7d710a4c8df0d
Ivan Budiselic <ibudiselic@gmail.com>
2020-04-09 09:13:58 +0200

    Update data.

---
+++ to_html_and_push.ps1
@@ -0,0 +1,10 @@
+jupyter nbconvert analyze.ipynb --to html --output analyze.html
+python fix_html.py
+git add .
+git commit -m "Update data."
+git push origin master
+git checkout gh-pages
+git merge master
+git push origin gh-pages
+git checkout master
+pause
