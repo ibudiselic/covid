ff9cf677ee3bc4efa1c675135ed3cde34da188b5
Ivan Budiselic <ibudiselic@gmail.com>
2020-04-19 10:33:33 +0200

    Switch back to inline plots to make CLI execution easier.

--- to_html_and_push.ps1
+++ to_html_and_push.ps1
@@ -3,7 +3,7 @@ python fix_html.py analyze.html
 jupyter nbconvert croatia-nordics-and-some.ipynb --to html --output croatia-nordics-and-some.html
 python fix_html.py croatia-nordics-and-some.html
 git add .
-git commit -m "Update data."
+git commit -m "Switch back to inline plots to make CLI execution easier."
 git push origin master
 git checkout gh-pages
 git merge master
